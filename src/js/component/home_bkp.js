33472d70e4e2d703e18d7b3494346d08d74d65cd
Daniel Ipince <ipince22@gmail.com>
2021-02-12 06:00:32 +0000

    sample20210

--- src/js/component/home_bkp.js
+++ src/js/component/home_bkp.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
 
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
 export function Home() {
 	const [list, setList] = useState([]);
 	const [content, setContent] = useState();
 
+	const [count, setCount] = useState(0);
 	return (
 		<>
-			<div className="text-center mt-5">
-				<h1>List To Do</h1>
+			<div className="text-center pt-5 mt-5">
+				<h1>todos</h1>
 				<input
 					key={"txt1"}
 					value={content}
-					onChange={e => {
-						setContent(e.target.value.toLocaleUpperCase());
-					}}
+					onChange={e => setContent(e.target.value.toUpperCase())}
 					onKeyPress={e => {
 						if (e.key === "Enter") {
 							if (content !== "") {
-								setList(
-									list.concat(content.toLocaleUpperCase())
-								);
+								setList(list.concat(content.toUpperCase()));
 								setContent("");
 							} else {
-								alert("input one task");
+								alert("input one text,please!");
 							}
 						}
 					}}
@@ -30,33 +31,39 @@ export function Home() {
 				<button
 					onClick={() => {
 						if (content !== "") {
-							setList(list.concat(content.toLocaleUpperCase()));
+							setList(list.concat(content.toUpperCase()));
 							setContent("");
 						} else {
-							alert("input one task");
+							alert("input one text,please!");
 						}
 					}}>
-					Add
+					add to do
 				</button>
 				{list.map((item, index) => {
 					return (
 						<div
-							className="list-group-item list-group-item-action"
 							key={index}
-							onClick={() => {
+							onClick={() =>
 								setList(
 									list.filter(
 										(itemf, indexf) => indexf !== index
 									)
-								);
-							}}>
+								)
+							}
+							className="list-group-item list-group-item-action">
 							{item}
 						</div>
 					);
 				})}
 				<br />
-				<cite>{"Total de elemetos: " + list.length}</cite>
+				<cite>{"* " + list.length + " items left"} </cite>
 			</div>
 		</>
 	);
 }
+
+/*
+    window.onload = function() {
+	document.getElementById("#txt1").focus();
+};
+*/
